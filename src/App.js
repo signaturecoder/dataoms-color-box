81d23ebdac248673baaeaf32f65b07faacb2a478
sanu kumar <snkmr13@gmail.com>
2021-09-10 16:43:03 +0530

    landing page

--- src/App.js
+++ src/App.js
@@ -1,41 +1,12 @@
 import React, { useState } from "react";
 import "./App.css";
-import Box from "../src/components/Box";
+import SideDrawer from "./components/SideDrawer";
 
 function App() {
-  const [bgColor, setBgColor] = useState("orange");
-  const [boxNum, setBoxNum] = useState(0);
-  const [boxes, setBoxes] = useState(Array.from(Array(9).keys()));
 
-  const handleInputChange = (e) => {
-    setBoxNum(e.target.value);
-  };
-
-  const handleColor = (event) => {
-    const dublicateBoxes = [...boxes];
-   const matchedBox = dublicateBoxes.find((box) => box === boxNum)
-   setBoxes(dublicateBoxes);
-    console.log(matchedBox);
-    console.log("event", dublicateBoxes);
-     event.preventDefault();
-  };
   return (
     <div className="App">
-      <input
-        type="number"
-        placeholder="Enter Number"
-        onChange={handleInputChange}
-        value={boxNum}
-      ></input>
-      <button
-        style={{ width: "100px", height: "50px", borderRadius: "25px" }}
-        onClick={handleColor}
-      >
-        Color Me
-      </button>
-      {boxes.map((boxItem, index) => (
-        <Box key={index} boxNum={index + 1} />
-      ))}
+     <SideDrawer />
     </div>
   );
 }
---
+++ src/components/BadgeAvatar.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import Badge from '@material-ui/core/Badge';
+import Avatar from '@material-ui/core/Avatar';
+import { makeStyles, withStyles } from '@material-ui/core/styles';
+
+
+const SmallAvatar = withStyles((theme) => ({
+  root: {
+    width: 22,
+    height: 22,
+    border: `2px solid ${theme.palette.background.paper}`,
+  },
+}))(Avatar);
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: 'flex',
+    '& > *': {
+      margin: theme.spacing(1),
+
+    },
+    justifyContent: 'center'
+  },
+  badge: {
+    fontSize: 45
+  }
+}));
+
+export default function BadgeAvatar() {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+
+      <Badge
+      className={classes.badge}
+        overlap="circular"
+        anchorOrigin={{
+          vertical: 'bottom',
+          horizontal: 'right',
+        }}
+        badgeContent={<SmallAvatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" />}
+      >
+        <Avatar alt="Travis Howard" src="/static/images/avatar/2.jpg" />
+      </Badge>
+    </div>
+  );
+}
